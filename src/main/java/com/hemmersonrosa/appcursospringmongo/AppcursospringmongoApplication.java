ecd4c184ca98afa85b5dd645144f1ddce6c90e9f
hemmerson <hemmerson.rosa@gmail.com>
2024-03-06 00:45:20 -0300

    first commit

---
+++ src/main/java/com/hemmersonrosa/appcursospringmongo/AppcursospringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.hemmersonrosa.appcursospringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcursospringmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppcursospringmongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemmersonrosa/appcursospringmongo/AppcursospringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemmersonrosa.appcursospringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcursospringmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
